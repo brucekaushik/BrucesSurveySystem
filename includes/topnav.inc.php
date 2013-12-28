0c2b7449e70f38891bd21683844bb972e6817f51
Kaushik <kaushik.srinivas@gmail.com>
2013-12-28 12:13:11 +0530

    adding missing files from last commit

---
+++ includes/topnav.inc.php
@@ -0,0 +1,5 @@
+<div class="topnav">
+	<a href="../BrucesAdminArea/index.php">Home</a> |
+	<a href="index.php">SurveySystem Home</a> |
+	<a href="../BrucesAdminArea/home.php?action=logout">Logout</a>
+</div>
