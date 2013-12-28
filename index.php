c9afeff5a97e765da819d2548f214ed100883c72
Kaushik <kaushik.srinivas@gmail.com>
2013-12-28 11:49:32 +0530

    authenticate with BrucesAdminArea and redirect back

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
-header("Location: ../08-adminArea");
+session_start();
+
+$_SESSION['verify_back_to'] = "../BrucesSurveySystem/index2.php";
+$_SESSION['app_name'] = "Bruce's Survey System";
+
+// redirect to home page
+header ("Location: ../BrucesAdminArea/index.php?action=VerifyLogin");
 
 ?>
